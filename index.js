8a91143dad719d1b0c503cf875550459439c9507
JSA <sunderganeshk@gmail.com>
2021-05-10 10:20:43 +0530

    from home to git

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello World!"));
+app.get("/", (req, res) => res.send("Hello this is change made from home to GIT!"));
 
 app.listen(3000, () => console.log("Server listening on port 3000!"));
