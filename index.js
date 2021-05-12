dee93506362bdc0bdf1a7e1dd052f8357eef190e
JSA <sunderganeshk@gmail.com>
2021-05-12 19:20:14 +0530

    Changes done locally

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const PORT = process.env.PORT || 5000;
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello this is change made from home to GIT!"));
+app.get("/", (req, res) => res.send("Hello this is change made from local to remote!"));
+
 
 app.listen(PORT, () => console.log("Server listening on port !"));
