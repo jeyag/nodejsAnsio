081985a9199894d485e759996a9fd7d9c89ce378
JSA <sunderganeshk@gmail.com>
2021-05-12 20:11:52 +0530

    Changes done locally

--- index.js
+++ index.js
@@ -10,12 +10,7 @@ app.engine('hbs', hbs({extname: 'hbs', defaultLayout: 'layout', layoutsDir: __di
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-app.get("/", (req, res) => res.send("Hello this is change made from local to remote!"));
 
-// POST method route
-app.post("/post", function (req, res) {
-    res.send('POST request to the homepage');
-  });
 
 
 app.listen(PORT, () => console.log("Server listening on port !"));
