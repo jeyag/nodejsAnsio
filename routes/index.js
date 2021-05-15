412b3b60098fb9b34d6ae4167a181d6175af6391
JSA <sunderganeshk@gmail.com>
2021-05-15 15:15:02 +0530

    get route tested

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const { json } = require('body-parser');
 var express = require('express');
 var router = express.Router();
 
@@ -10,9 +11,24 @@ router.get('/test/:empNo', function(req, res, next) {
   res.render('test', {output: req.params.empNo});
 });
 
+
 router.post('/test/submit', function(req, res, next) {
- 
-res.redirect('/test/' + req.body.empNo);
+  //var data = JSON.parse(body);
+  var data = {
+    "Employee": {
+        "id": req.body.empNo,
+        "fname": req.body.fname,
+        "lname":req.body.lname,
+        "team":req.body.team,
+        "feedback":req.body.feedback
+    }
+  }; 
+  
+ /* return res
+          .status(201)
+          .json(data);*/
+
+res.redirect('/test/'+ data.Employee.id );
 });
 
 module.exports = router;
